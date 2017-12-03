7d24356285297cceb5160bc68710537e52506936
Joshua Miller <joshua@egauge.net>
2017-12-02 18:38:27 -0800

    Support reverse() import change

--- private_storage/storage/files.py
+++ private_storage/storage/files.py
@@ -2,9 +2,13 @@
 Django Storage interface, using the file system backend.
 """
 from django.core.files.storage import FileSystemStorage
-from django.core.urlresolvers import reverse_lazy
 from django.utils.deconstruct import deconstructible
 from django.utils.encoding import force_text
+
+try:
+    from django.urls import reverse_lazy  # Added in Django 1.10
+except ImportError:
+    from django.core.urlresolvers import reverse_lazy
 
 from private_storage import appconfig
 
