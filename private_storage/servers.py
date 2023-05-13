f80716f6086ba547419288afa88f7be7a91520f5
Antonio Morillas <amorilla@xtec.cat>
2023-05-13 13:54:55 +0200

    For Actions testing

--- private_storage/servers.py
+++ private_storage/servers.py
@@ -79,7 +79,7 @@ class DjangoStreamingServer:
         else:
             response = FileResponse(private_file.open())
         response['Content-Type'] = private_file.content_type
-        response['Content-Length'] = private_file.size
+        response['Content-Length'] = size
         response["Last-Modified"] = http_date(mtime)
         return response
 
